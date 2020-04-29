6d58056033ad7a29e2393d026df01dc6e2c95936
summerdebug <summerdebug@gmail.com>
2020-04-29 17:49:54 +0300

    for cycle direction changed

--- src/main/java/com/algorithm/array/sort/BubbleSort.java
+++ src/main/java/com/algorithm/array/sort/BubbleSort.java
@@ -4,7 +4,7 @@ class BubbleSort {
 
     static void sort(int[] values) {
         for (int i = 0; i < values.length - 1; i++) {
-            for (int j = i + 1; j < values.length; j++) {
+            for (int j = values.length - 1; j > i; j--) {
                 if (values[j] < values[i]) {
                     swap(values, i, j);
                 }
